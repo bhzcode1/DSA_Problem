8e7dbe56dd11d2c8781cf090d8ec9e4e4be440cd
BHAVESH SEERVI <164318855+bhzcode1@users.noreply.github.com>
2026-07-24 00:52:46 +0530

    Time: 11 ms (83.44%), Space: 19.3 MB (26.61%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+
+public:
+    Node* connect(Node* root) {
+         if (root == NULL) return NULL;
+     queue<Node*> q;
+     q.push(root);
+
+    while (!q.empty()) {
+        // Track the number of nodes at the current level
+        int levelSize = q.size();
+
+        // Process all nodes of the current level
+        for (int i = 0; i < levelSize; i++) {
+            Node* node = q.front();
+            q.pop();
+             if(i == levelSize-1){
+                node->next = NULL;
+             }
+             else{
+                node->next = q.front();
+             }
+            // Enqueue left child
+            if (node->left != NULL) {
+                q.push(node->left);
+            }
+            // Enqueue right child
+            if (node->right != NULL) {
+                q.push(node->right);
+            }
+        }
+      
+    }
+        return root;
+    }
+};
